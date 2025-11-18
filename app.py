dc93e72280dbeb8a537e03849d72c48936f00eb8
keoto-14 <khkhairul27@gmail.com>
2025-11-18 16:14:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
-from predictor import predictor_router, Predictor
+from predictor import  Predictor
 
 load_dotenv()
 
